826b718a0ebfe3d05c8b89f936fe091c6ca9c092
Rubes0801 <222055@trinityschools.nz>
2025-05-14 12:58:09 +1200

    Made new login page, deleted old way of doing this and now need to add the authenication, validation and CSS.

--- app.py
+++ app.py
@@ -5,19 +5,6 @@ from flask_migrate import Migrate
 
 db = SQLAlchemy()
 
-def create_app():
-    app = Flask(__name__, template_folder='templates')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./testdb.db'
-
-    db.init_app(app)
-
-    from routes import register_routes
-    register_routes(app, db)
-
-    migrate = Migrate(app, db)
-
-    return app
-
 app = Flask(__name__)
 
 DATABASE = 'NZ_Wildlife.db'
